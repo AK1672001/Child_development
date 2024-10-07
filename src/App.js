25c30ec330dac3b79b8a77162105aadccc68766f
Your Name <you@example.com>
2024-10-07 21:03:55 +0530

    add build folder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-
 import Navbar from "./Component/Navbar";
-import { Routes,Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Udeshya from "./Component/Udeshya";
 import Homepage from "./Pages/Home";
 import ContactPage from "./Component/Contact";
 function App() {
   return (
     <>
-      <Navbar/>
-       <Routes>
-         <Route path='/' element={<Udeshya/>}></Route>
-
-         <Route path='/udeshya' element={<Udeshya/>}></Route>
-         <Route path='/contact' element={<ContactPage/>}></Route>
-       </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Udeshya />}></Route>
+        <Route path="/udeshya" element={<Udeshya />}></Route>
+        <Route path="/contact" element={<ContactPage />}></Route>
+      </Routes>
     </>
   );
 }
